35506bd6d0edb04715e8422395b09dbec53a934d
sTorov <sotorov2011@yandex.ru>
2022-07-22 01:34:35 +0300

    Task1 Переделан

--- Task1/Calculator.cs
+++ Task1/Calculator.cs
@@ -1,10 +1,50 @@
 ﻿namespace Task1
 {
+    delegate double CheckValueDelegate(int oper);
+
     class Calculator : ICalculator
     {
+        private double Number1 { get; set; }
+        private double Number2 { get; set; }
+
         public void Sum(double numberOne, double numberTwo)
         {
-            Console.WriteLine("\nОтвет: " + Math.Round((numberOne + numberTwo), 6, MidpointRounding.AwayFromZero));
+            Console.WriteLine("Ответ: " + Math.Round((numberOne + numberTwo), 6, MidpointRounding.AwayFromZero));
+        }
+
+        public void ReadValue()
+        {
+            CheckValueDelegate checkValue = (int oper) =>
+            {
+                while (true)
+                {
+                    Console.Write($"Введите {oper}-ю цифру: ");
+                    try
+                    {
+                        if (double.TryParse(Console.ReadLine(), out double result))
+                            return result;
+                        else
+                            throw new Exception("Ошибка! Неверное значение. Попробуйте снова.");
+                    }
+                    catch (Exception e)
+                    {
+                        PrintException(e);
+                        continue;
+                    }
+                }
+            };
+
+            Number1 = checkValue(1);
+            Number2 = checkValue(2);
+
+            Sum(Number1, Number2);
+        }
+
+        private void PrintException(Exception e)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(e.Message);
+            Console.ResetColor();
         }
     }
 }
--- Task1/Program.cs
+++ Task1/Program.cs
@@ -4,43 +4,24 @@
     {
         static void Main(string[] args)
         {
-            NumberReader numberReader = new NumberReader();
             Calculator calculator = new Calculator();
-            numberReader.readNumberEvent += calculator.Sum;
 
             while (true)
             {
-                try
-                {
-                    numberReader.ReadValue();
+                calculator.ReadValue();
 
-                    Console.WriteLine("\nДля продолжения нажмите 'y'\nДля выхода нажмите любую другую клавишу");
-                    char ch = Console.ReadKey(true).KeyChar;
-                    switch (ch)
-                    {
-                        case 'y':
-                            Console.Clear();
-                            continue;
-                        default:
-                            break;
-                    }
-
-                    break;
-                }
-                catch (Exception e)
+                Console.WriteLine("\nДля продолжения нажмите 'y'\nДля выхода нажмите любую другую клавишу");
+                char ch = Console.ReadKey(true).KeyChar;
+                switch (ch)
                 {
-                    PrintException(e);
-                    Console.ReadKey();
-                    Console.Clear();
+                    case 'y':
+                        Console.Clear();
+                        continue;
+                    default:
+                        break;
                 }
+                break;                
             }            
-        }
-
-        static void PrintException(Exception e)
-        {
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine("\n" + e.Message);
-            Console.ResetColor();
-        }
+        }       
     }
 }
